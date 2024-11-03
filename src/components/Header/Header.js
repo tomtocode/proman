44f6abb8746d37cb5b6366b989a682e7e015a2f2
Tomasz <tomaszsiemaszko@gmail.com>
2024-11-02 22:29:46 -0400

    no wrap phone number

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
                     <Row>
                         <Col>
                             <p className="pe-paragraph" style={{ textAlign: "center", color: "white", fontWeight: "bold", fontSize: "medium", verticalAlign: "bottom", paddingBottom: "0px", marginBottom: "0px" }}>
-                                Established 2010 | NJ License #16105 | <a style={{ color: "#ef5611" }} href="tel:732-851-4822">(732) 851-4822 </a> | <a style={{ color: "#ef5611" }} href="mailto:promanelectric@gmail.com">promanelectric@gmail.com</a>
+                                Established 2010 | NJ License #16105 | <a style={{ color: "#ef5611", whiteSpace:"nowrap" }} href="tel:732-851-4822">(732) 851-4822 </a> | <a style={{ color: "#ef5611" }} href="mailto:promanelectric@gmail.com">promanelectric@gmail.com</a>
                             </p>
                         </Col>
                     </Row>
