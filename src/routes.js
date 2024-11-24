8ada00d3847d0a3ccfeed6b31e907442d58c1e21
Tomasz <tomaszsiemaszko@gmail.com>
2024-11-24 00:26:52 -0500

    add site map

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Home } from './components/Home';
 import { About } from './components/About';
-import {Portfolio} from './components/Portfolio';
-import {Contact} from './components/Contact';
 import { Services } from './components/Services';
-import { NotFound } from './components/NotFound';
 
 
 import { Route, Switch, Redirect } from 'react-router-dom';
@@ -19,8 +16,6 @@ export const Routes = () => {
         </Route>
         <Route path="/about" component={About} />
         <Route path="/services" component={Services} />
-        {/* <Route path="/portfolio" component={Portfolio} /> */}
-        <Route path="/contact-us" component={Contact} />
         <Route component={Home} />
       </Switch>
     </div>
