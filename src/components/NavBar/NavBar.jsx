064cc8c78b533d1d6c8714586c7b2578b7fa3f85
Tomasz <tomaszsiemaszko@gmail.com>
2026-06-10 20:44:50 -0400

    remove extra padding

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,13 +6,13 @@ import './NavBar.css';
 const NavBar = () => {
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" variant="dark" className="px-2">
-        <Container>
+      <Navbar collapseOnSelect expand="lg" variant="dark" className="px-0">
+        <Container fluid className="px-0">
           {/* <Navbar.Brand href="#home">PROMAN</Navbar.Brand> */}
           {/* TODO: Use # navigation */}
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" className="ms-auto" />
-          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-            <Nav className="me-auto align-items-start align-items-lg-center">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" className="ms-auto me-2" />
+          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end w-100 navbar-collapse-custom">
+            <Nav className="ms-auto align-items-start align-items-lg-center nav-custom">
               <NavLink exact to="/" activeClassName="main-nav-active" style={{ color: "white", fontWeight: "bold" }}>Home</NavLink>
               <NavLink exact to="/services" activeClassName="main-nav-active" style={{ color: "white", fontWeight: "bold", marginLeft: "20px" }}>Services</NavLink>
               <NavLink exact to="/about" activeClassName="main-nav-active" style={{ color: "white", fontWeight: "bold", marginLeft: "20px" }}>About Us</NavLink>
